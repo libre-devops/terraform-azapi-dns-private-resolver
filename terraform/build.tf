fa5ef792da92dcc9989f88c3a5a748702fdcac6b
Craig Thacker <craigthackerx@gmail.com>
2022-10-16 03:13:09 +0100

    Update module

--- terraform/build.tf
+++ terraform/build.tf
@@ -19,7 +19,7 @@ module "network" {
   vnet_location = module.network.vnet_location
 
   address_space   = ["10.0.0.0/16"]
-  subnet_prefixes = ["10.0.1.0/24", "10.0.17.0/24", "10.0.18/24"]
+  subnet_prefixes = ["10.0.1.0/24", "10.0.17.0/24", "10.0.18.0/24"]
   subnet_names    = ["sn1-${module.network.vnet_name}", "sn2-${module.network.vnet_name}", "sn3-${module.network.vnet_name}"] //sn1-vnet-ldo-euw-dev-01
   subnet_service_endpoints = {
     "sn1-${module.network.vnet_name}" = ["Microsoft.Storage"]                   // Adds extra subnet endpoints to sn1-vnet-ldo-euw-dev-01
